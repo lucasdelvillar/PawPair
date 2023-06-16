65bc9b5d4f17f8b4e1d8a03ccb078e5bd0857524
Lucas <lucasdelvillar14@gmail.com>
2023-06-15 19:29:38 -0700

    added structure and styles for the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,52 @@
 import React from 'react';
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import { BiHeartCircle } from "react-icons/bi";
 
 function Home() {
+    function Buttons() {
+        return (
+            <>
+                <AiOutlineCloseCircle />
+                <BiHeartCircle />
+            </>
+        )
+    }
+
+    function OtherUserInfo() {
+        return (
+            <>
+                <span className="otherUserName">Kai, 2</span>
+                <span className="otherUserLoc">Vancouver, 1km</span>
+                <span className>Bio</span>
+                <span className>Hey my name is Kai, and I am a super enegetic dog!</span>
+            </>
+        )
+    }
+
+    function Card() {
+        return (
+            <>
+                <img className="otherUserPhoto" src={require("../images/kai_photo.png")} alt="user photo" />
+                <div className="otherUserInfo">
+                    <OtherUserInfo />
+                </div>
+                <div className="like-or-dislike">
+                    <Buttons />
+                </div>
+            </>
+        )
+    }
+
     return (
         <>
             <h2 className="logo">PawPair</h2>
-            <h1>This is the homepage</h1>
+            <div className="home-container">
+                <h1>Pets Nearby</h1>
+                <div className='card-container'>
+                    <Card/>
+                </div>
+            </div>
         </>
-        
     );
 }
 
